ebdaf58c119ee48d75194eabb3da92b247b55693
rodolfo <rodolfo.borbon@icloud.com>
2025-08-04 18:07:23 -0400

    total amount calculator added

--- calculateArea/calculate_area.js
+++ calculateArea/calculate_area.js
@@ -9,3 +9,17 @@ function calculateArea() {
 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
    }
+
+let banana;
+let milk;
+let chicken;
+
+function calculateTotalAmount() {
+    banana = parseFloat(document.getElementById('banana').value);
+    milk = parseFloat(document.getElementById('milk').value);
+    chicken = parseFloat(document.getElementById('chicken').value);
+
+    let total = banana + milk + chicken;
+
+    document.getElementById('result2').innerText = `The total amount is: \$ ${total}`;
+}
